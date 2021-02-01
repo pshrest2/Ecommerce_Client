0c3f55b992450716cad81e51cf82190483307fdb
Pranaya Shrestha <pshrest2@go.olemiss.edu>
2021-02-01 17:39:54 -0600

    added layout page for Home, Signin and Signup pages

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Layout from "../core/Layout";
 
-const Signin = () => <div>signin</div>;
+const Signin = () => (
+  <Layout title="Sign In Page" description="Node React ECommerce App">
+    ...
+  </Layout>
+);
 
 export default Signin;
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Layout from "../core/Layout";
 
-const Signup = () => <div>signup</div>;
+const Signup = () => (
+  <Layout title="Sign Up Page" description="Node React ECommerce App">
+    ...
+  </Layout>
+);
 
 export default Signup;
