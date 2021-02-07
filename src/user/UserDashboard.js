b1e950604b6517957cc87f7cfc1bc5c3e3eeebbb
Pranaya Shrestha <pshrest2@go.olemiss.edu>
2021-02-07 11:52:51 -0600

    updated userdashboard

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -1,18 +1,34 @@
 import React from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
-
+import { Link } from "react-router-dom";
 const Dashboard = () => {
   const {
     user: { name, email, role },
   } = isAuthenticated();
 
-  return (
-    <Layout
-      title="Dashboard"
-      description="User Dashboard"
-      classname="container"
-    >
+  const userlinks = () => {
+    return (
+      <div className="card">
+        <h4 className="card-header">User Links</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <Link className="nav-link" to="/cart">
+              My Cart
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link" to="/profile/update">
+              Update Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  const userInfo = () => {
+    return (
       <div className="card mb-5">
         <h3 className="card-header">User Information</h3>
 
@@ -24,13 +40,29 @@ const Dashboard = () => {
           </li>
         </ul>
       </div>
+    );
+  };
 
+  const purchaseHistory = () => {
+    return (
       <div className="card mb-5">
         <h3 classname="card-header"> Purchase History </h3>
 
         <ul className="list-group">
           <li className="list-group-item">history</li>
         </ul>
+      </div>
+    );
+  };
+
+  return (
+    <Layout title="Dashboard" description={name} className="container-fluid">
+      <div className="row">
+        <div className="col-3">{userlinks()}</div>
+        <div className="col-9">
+          {userInfo()}
+          {purchaseHistory()}
+        </div>
       </div>
     </Layout>
   );
