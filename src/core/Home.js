075751497432b63393e12eb5743ad20bb0982b53
Pranaya Shrestha <pshrest2@go.olemiss.edu>
2021-02-16 19:04:52 -0600

    Home page displays products (by sell and by arrival). However, the data is in JSON form right now.

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { getProducts } from "./apiCore";
+const Home = () => {
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
 
-const Home = () => (
-  <Layout title="Home Page" description="Node React ECommerce App">
-    ...
-  </Layout>
-);
+  const loadProductsBySell = () => {
+    getProducts("sold").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsBySell(data);
+      }
+    });
+  };
+  const loadProductsByArrival = () => {
+    getProducts("createdAt").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsByArrival(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
+
+  return (
+    <Layout title="Home Page" description="Node React ECommerce App">
+      {JSON.stringify(productsByArrival)}
+      <hr />
+      {JSON.stringify(productsBySell)}
+    </Layout>
+  );
+};
 
 export default Home;
